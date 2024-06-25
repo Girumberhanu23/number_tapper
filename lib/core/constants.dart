480f40c82176922bffafbc409ff845d75de1f499
Girumberhanu23 <girumberhanugb4@gmail.com>
2024-06-25 23:13:57 +0300

    Ui/Ux Game Screen Design

---
+++ lib/core/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color white = Color(0xFFFFFFFF);
+Color black = Color(0xFF000000);
+Color darkBlue = Color(0xFF070424);
+Color blue = Color.fromARGB(255, 31, 139, 255);
